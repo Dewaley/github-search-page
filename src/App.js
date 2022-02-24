73998ec2374746133807a9dfab4166fd7f7a1e50
Dewaley <adewaleademilua@gmail.com>
2022-02-24 05:13:36 +0100

    Created and styled header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
+import Input from './components/Input';
+import {GoMarkGithub} from 'react-icons/go'
 
 function App() {
   return (
-    <div className="App">
-      Hello World
+    <div className='App'>
+      <header>
+        <h1>
+          <a href=''>
+            <GoMarkGithub className='logo' size={20}/>
+            <span>Github User Search</span>
+          </a>
+        </h1>
+        <Input />
+      </header>
     </div>
-  );
+  ); 
 }
 
 export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {GoSearch} from 'react-icons/go'
+
+const Input = () => {
+  return (
+    <div className='input'>
+     <input type="text" name="" id="" placeholder='Username' />
+     <GoSearch/>
+    </div>
+  )
+}
+
+export default Input
