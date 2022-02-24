f3978d49210f9a897d715608e8cc066c2fa16506
Dewaley <adewaleademilua@gmail.com>
2022-02-24 19:28:29 +0100

    Got api right

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
 import './App.css';
 import Input from './components/Input';
-import {GoMarkGithub} from 'react-icons/go'
-import {useState} from 'react'
+import { GoMarkGithub } from 'react-icons/go';
+import { useState } from 'react';
 
 function App() {
-  const [user,setUser]= useState('')
-  const [users,setUsers]=useState({})
-  const search = async() => {
-    const res = await fetch(`https://api.github.com/search/users?q=${user}`)
-    const data= await res.json()
-    setUsers(data)
-    console.log(data)
-  }
-  const submit =(e) => {
-    e.preventDefault()
-    search()
-  }
+  const [user, setUser] = useState('');
+  const [users, setUsers] = useState([]);
+  const search = async () => {
+    const res = await fetch(`https://api.github.com/search/users?q=${user}`);
+    const data = await res.json();
+    const response = data.items
+    setUsers(response);
+    console.log(response);
+  };
+  const submit = (e) => {
+    e.preventDefault();
+    search();
+  };
   return (
     <div className='App'>
       <header>
         <h1>
           <a href='google.com'>
-            <GoMarkGithub className='logo' size={20}/>
+            <GoMarkGithub className='logo' size={20} />
             <span>Github User Search</span>
           </a>
         </h1>
         <Input user={user} setUser={setUser} search={search} submit={submit} />
       </header>
-      <section>
-        {users.map(user => (
-          <div></div>
-        ))}
-      </section>
+      {users !== {} && (<section>
+        {users.map((profile) => {
+          return <div>{profile.id}</div>;
+        })}
+      </section>)}
     </div>
-  ); 
+  );
 }
 
 export default App;
